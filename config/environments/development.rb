83694218634d1b3c46f17aa12748fa05061dd77f
Logan Price <logandsprice@users.noreply.github.com>
2018-03-08 19:55:34 -0600

    Config erd, annotate, and bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,14 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Bullet configuration
+  Bullet.enable = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+  Bullet.rails_logger = true
+  Bullet.rollbar = true
+  Bullet.add_footer = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
