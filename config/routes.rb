bbc948c66cdee6a2c30e7b04dbfb4da3661a45ca
Logan Price <logandsprice@users.noreply.github.com>
2018-03-09 16:10:19 -0600

    Condense routes file. Restrict routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,137 +1,18 @@
 Rails.application.routes.draw do
-  # Routes for the User resource:
-
-  # CREATE
-  get("/users/new", { :controller => "users", :action => "new_form" })
-  post("/create_user", { :controller => "users", :action => "create_row" })
-
-  # READ
-  get("/users", { :controller => "users", :action => "index" })
-  get("/users/:id_to_display", { :controller => "users", :action => "show" })
-
-  # UPDATE
-  get("/users/:prefill_with_id/edit", { :controller => "users", :action => "edit_form" })
-  post("/update_user/:id_to_modify", { :controller => "users", :action => "update_row" })
-
-  # DELETE
-  get("/delete_user/:id_to_remove", { :controller => "users", :action => "destroy_row" })
-
-  #------------------------------
-
-  # Routes for the Resource resource:
-
-  # CREATE
-  get("/resources/new", { :controller => "resources", :action => "new_form" })
-  post("/create_resource", { :controller => "resources", :action => "create_row" })
-
-  # READ
-  get("/resources", { :controller => "resources", :action => "index" })
-  get("/resources/:id_to_display", { :controller => "resources", :action => "show" })
-
-  # UPDATE
-  get("/resources/:prefill_with_id/edit", { :controller => "resources", :action => "edit_form" })
-  post("/update_resource/:id_to_modify", { :controller => "resources", :action => "update_row" })
-
-  # DELETE
-  get("/delete_resource/:id_to_remove", { :controller => "resources", :action => "destroy_row" })
-
-  #------------------------------
-
-  # Routes for the Launch resource:
-
-  # CREATE
-  get("/launches/new", { :controller => "launches", :action => "new_form" })
-  post("/create_launch", { :controller => "launches", :action => "create_row" })
-
-  # READ
-  get("/launches", { :controller => "launches", :action => "index" })
-  get("/launches/:id_to_display", { :controller => "launches", :action => "show" })
-
-  # UPDATE
-  get("/launches/:prefill_with_id/edit", { :controller => "launches", :action => "edit_form" })
-  post("/update_launch/:id_to_modify", { :controller => "launches", :action => "update_row" })
-
-  # DELETE
-  get("/delete_launch/:id_to_remove", { :controller => "launches", :action => "destroy_row" })
-
-  #------------------------------
-
-  # Routes for the Enrollment resource:
-
-  # CREATE
-  get("/enrollments/new", { :controller => "enrollments", :action => "new_form" })
-  post("/create_enrollment", { :controller => "enrollments", :action => "create_row" })
-
-  # READ
-  get("/enrollments", { :controller => "enrollments", :action => "index" })
-  get("/enrollments/:id_to_display", { :controller => "enrollments", :action => "show" })
-
-  # UPDATE
-  get("/enrollments/:prefill_with_id/edit", { :controller => "enrollments", :action => "edit_form" })
-  post("/update_enrollment/:id_to_modify", { :controller => "enrollments", :action => "update_row" })
-
-  # DELETE
-  get("/delete_enrollment/:id_to_remove", { :controller => "enrollments", :action => "destroy_row" })
-
-  #------------------------------
-
-  # Routes for the Credential resource:
-
-  # CREATE
-  get("/credentials/new", { :controller => "credentials", :action => "new_form" })
-  post("/create_credential", { :controller => "credentials", :action => "create_row" })
-
-  # READ
-  get("/credentials", { :controller => "credentials", :action => "index" })
-  get("/credentials/:id_to_display", { :controller => "credentials", :action => "show" })
-
-  # UPDATE
-  get("/credentials/:prefill_with_id/edit", { :controller => "credentials", :action => "edit_form" })
-  post("/update_credential/:id_to_modify", { :controller => "credentials", :action => "update_row" })
-
-  # DELETE
-  get("/delete_credential/:id_to_remove", { :controller => "credentials", :action => "destroy_row" })
-
-  #------------------------------
-
-  # Routes for the Context resource:
-
-  # CREATE
-  get("/contexts/new", { :controller => "contexts", :action => "new_form" })
-  post("/create_context", { :controller => "contexts", :action => "create_row" })
-
-  # READ
-  get("/contexts", { :controller => "contexts", :action => "index" })
-  get("/contexts/:id_to_display", { :controller => "contexts", :action => "show" })
-
-  # UPDATE
-  get("/contexts/:prefill_with_id/edit", { :controller => "contexts", :action => "edit_form" })
-  post("/update_context/:id_to_modify", { :controller => "contexts", :action => "update_row" })
-
-  # DELETE
-  get("/delete_context/:id_to_remove", { :controller => "contexts", :action => "destroy_row" })
-
-  #------------------------------
-
-  # Routes for the Check in resource:
-
-  # CREATE
-  get("/check_ins/new", { :controller => "check_ins", :action => "new_form" })
-  post("/create_check_in", { :controller => "check_ins", :action => "create_row" })
-
-  # READ
-  get("/check_ins", { :controller => "check_ins", :action => "index" })
-  get("/check_ins/:id_to_display", { :controller => "check_ins", :action => "show" })
-
-  # UPDATE
-  get("/check_ins/:prefill_with_id/edit", { :controller => "check_ins", :action => "edit_form" })
-  post("/update_check_in/:id_to_modify", { :controller => "check_ins", :action => "update_row" })
-
-  # DELETE
-  get("/delete_check_in/:id_to_remove", { :controller => "check_ins", :action => "destroy_row" })
-
-  #------------------------------
-
   devise_for :administrators
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :administrator do
+    root "credentials#index"
+  end
+
+  root "credentials#index"
+
+  resources :check_ins, only: [:create, :destroy]
+  resources :contexts, only: [:index]
+  resources :credentials, only: [:index, :create, :destroy]
+  resources :enrollments, only: []
+  # resources :launches, except: :create
+  resource  :launch, only: :create
+  resources :resources, only: :show
+  resources :users, only: []
 end
