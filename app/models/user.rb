3de467cc4cd5db367ff367dc0890029b24487e1e
Logan Price <logandsprice@users.noreply.github.com>
2018-03-21 14:21:55 -0500

    Trivial change to create PR

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@
 
 class User < ApplicationRecord
   has_many :enrollments, dependent: :destroy
-  has_many :check_ins, through: :enrollments
 
+  has_many :check_ins, through: :enrollments
   has_many :contexts, through: :enrollments
   has_many :launches, through: :enrollments
 
