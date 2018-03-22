f3c4c2189fcc2759bf60c627abe036c52b2488bc
Logan Price <logandsprice@users.noreply.github.com>
2018-03-22 13:08:28 -0500

    Stubbing out logic

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -14,4 +14,30 @@
 class Resource < ApplicationRecord
   belongs_to :context
   has_many :enrollments, dependent: :destroy
+
+  # add a column starts_on
+  # add a column ends_on
+  # add a column days_of_week
+
+  validate :must_have_schedule, on: :update
+
+  def must_have_schedule
+    if starts_on.blank? || ends_on.blank? || days_of_week.blank?
+      errors.add(:base, "You have to provide a schedule")
+    end
+  end
+
+  def meets_today?
+    # returns true or false
+  end
+
+  def all_occurrences
+    # returns an array of Date
+  end
+
+  after_update :create_meetings
+
+  def create_meetings
+
+  end
 end
