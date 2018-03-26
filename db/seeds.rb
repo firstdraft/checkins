2a30a382bbcaf597edfd9af3c00bd003fdd9f709
Logan Price <logandsprice@users.noreply.github.com>
2018-03-26 16:33:36 -0500

    seeds for first context and resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,17 @@ end
 if admin.credentials.empty?
   admin.credentials.create(consumer_key:"key", consumer_secret:"secret")
 end
+
+context = Context.find_or_create_by(id: 1) do |c|
+  c.title          = "Context ONE"
+  c.lti_context_id = "ContextONE"
+  c.credential_id  = 1
+end
+
+resource = context.resources.find_or_create_by(id: 1) do |r|
+  r.lis_outcome_service_url = "http://127.0.0.1:9393/grade_passback"
+  r.lti_resource_link_id = "ResourceONE"
+  r.starts_on = Date.new(2018,1,1)
+  r.ends_on = Date.new(2018,1,31)
+  r.days_of_week = [1,3,5]
+end
