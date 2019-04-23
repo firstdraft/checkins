50b5bcde53d4a7fce6c481462e7b4686a89c8482
Logan Price <logandsprice@gmail.com>
2019-04-23 12:42:11 -0500

    Implement AJAX for event buttons on attendances
    Add capybara gem
    
    Comment out prewritten tests
    
    Implement AJAX for events on individual attendances
    
    Remove capybara gem
    
    Undo comments to spec files
    
    This branch started off as a branch to implement some tests. I commented
    out a bunch of lines in spec files because I wanted to start fresh
    without some prewritten tests. Now that this branch has nothing to do
    with tests, I'm uncommenting.
    
    Remove commented line
    
    Hound suggestion

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,13 +1,18 @@
 class AttendancesController < ApplicationController
   def transition
-    attendance = Attendance.find(params[:attendance_id])
+    @attendance = Attendance.find(params[:attendance_id])
     event = params.fetch(:event).to_sym
 
-    if attendance.aasm.events.map(&:name).include?(event) &&
-        attendance.send(event)
-      attendance.save
+    if @attendance.aasm.events.map(&:name).include?(event) &&
+        @attendance.send(event)
+      @attendance.save
     end
+    request.variant = if current_enrollment.teacher?
+                        :teacher
+                      else
+                        :student
+                      end
 
-    redirect_to resource_path(attendance.resource)
+    @meeting = @attendance.meeting
   end
 end
